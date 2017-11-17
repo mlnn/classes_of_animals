7d9d9be8e21c128b65121e2037415fc53dc0f2ec
Andrey Anisimov <31520093+mlnn@users.noreply.github.com>
2017-11-17 14:39:58 +0300

    Update classes_of_animals.py

--- classes_of_animals.py
+++ classes_of_animals.py
@@ -1,31 +1,59 @@
 class Animal(object):
-  name = ''
+    name, sound = '', ''
+  
+    def __init__(self, name):
+        self.name = name
+
+    def say(self):
+        return self.sound
 
 
 class Mammal(Animal):
-  type = 'Млекопитающее'
-  def __init__(self, name):
-    self.name = name
+    pass
 
 
 class Bird(Animal):
-  type = 'Птица'
-  def __init__(self, name):
-    self.name = name
+    pass
 
 
-cow = Mammal(name='Зина')
-goat = Mammal(name='Машка')
-sheep = Mammal(name='Рита')
-pig = Mammal(name='Борис')
-duck = Bird(name='Кира')
-chicken = Bird(name='Глаша')
-goose = Bird(name='Ганс')
+class Cow(Mammal):
+    sound = 'Мууу'
 
-print(cow.type, cow.name)
-print(goat.type, goat.name)
-print(sheep.type, sheep.name)
-print(pig.type, pig.name)
-print(duck.type, duck.name)
-print(chicken.type, chicken.name)
-print(goose.type, goose.name)
+
+class Goat(Mammal):
+    sound = 'Мее'
+
+
+class Pig(Mammal):
+    sound = 'Хрю'
+    
+
+class Sheep(Mammal):
+    sound = 'Бее'
+
+
+class Duck(Bird):
+    sound = 'Кря'
+
+
+class Chicken(Bird):
+    sound = 'Кукареку'
+    
+
+class Goose(Bird):
+    sound = 'Куа'
+
+
+my_cow = Cow(name='Зина')
+my_pig = Pig(name='Борис')
+my_chicken = Chicken(name='Глаша')
+my_goat = Goat(name='Машка')
+my_sheep = Sheep(name='Рита')
+my_duck = Duck(name='Кира')
+my_goose = Goose(name='Ганс')
+
+for i in [my_cow, my_pig, my_chicken, my_goat, my_sheep, my_duck, my_goose]:
+    if isinstance(i, Bird):
+        print('Я птица, меня зовут {}, я говорю {}'.format(i.name, i.say()))
+    if isinstance(i, Mammal):
+        print('Я млекопитающее, меня зовут {}, я говорю {}'.format(i.name, i.say()))
